bf69d19a004969a7fe2fa8ea236f25e5a458dfa3
terrys48 <lp.kostakis@yahoo.gr>
2023-01-24 15:08:28 +0200

    3.5--completed

--- training-day3/swift_training_day3.playground/Contents.swift
+++ training-day3/swift_training_day3.playground/Contents.swift
@@ -205,6 +205,7 @@ extension String {
 
 class Calculator {
     var arithmeticOperator: String = ""
+    
     func arithmeticOperation() -> (Int, Int) -> Int {
         
         func multiplication(number1 int1: Int, number2 int2: Int) -> Int {
@@ -230,22 +231,10 @@ class Calculator {
         let firstOperand = Int(expression[expression.startIndex...expression.index(before: operSymbolIndex)])
         let secondOperand = Int(expression[operSymbolIndex...])
         arithmeticOperator = String(operSymbol)
-        arithmeticOperator
         var result = arithmeticOperation()
         return result(firstOperand!, secondOperand!)
     }
 }
-var x = Calculator()
-x.calculate("32+12")
-x.calculate("321*2")
-
-var y = Calculator()
-
-
-
-
-
-
 
 //3.5 Points & Rectangles
 //Create a type that for a Point coordinate, when create a type that for a Rectangle (use the Point)
@@ -253,6 +242,101 @@ var y = Calculator()
 //Add a pair of methods(mutating and non-mutating that will check if two Rectangles overlap
 //Add a method that will return the distance of a Point from the Axis centre 0,0
 //Add a method that will return the distance of a Point from an other Point
+
+struct Point {
+    var x = 0.0, y = 0.0
+}
+
+struct Size {
+    var width = 0.0, height = 0.0
+}
+
+struct Rectangle {
+    var size = Size()
+    var origin = Point()
+    // when having the origin Point and size we can calculate the center each time
+    var center: Point {
+        get {
+            let centerX = origin.x + (size.width / 2)
+            let centerY = origin.y + (size.height / 2)
+            return Point(x: centerX, y: centerY)
+        }
+        set(newCenter) {
+            origin.x = newCenter.x - (size.width / 2)
+            origin.y = newCenter.y - (size.height / 2)
+        }
+    }
+    // Move rectangle center to new Point with mutating func, recalculating self.origin
+    mutating func moveBy(x deltaX: Double, y deltaY: Double) {
+        origin = Point(x: deltaX - (size.width / 2), y: deltaY - (size.height / 2))
+    }
+    
+    // non-mutating func that checks if two rectangles overlap
+    func overlaps(_ secondRectangle: Rectangle) -> Bool {
+        // Bottom left Point is the origin
+        var firstOrigin = Point(x: self.origin.x, y: self.origin.y)
+        var secondOrigin = Point(x: secondRectangle.origin.x, y: secondRectangle.origin.y)
+        
+        // take the top right Point that creates the diagonal from origin to that Point
+        var firstTopRight = getTopRightPoint(self)
+        var secondTopRight = getTopRightPoint(secondRectangle)
+
+        // If rectangle has area 0, no overlap
+        if (firstOrigin.x == firstTopRight.x || firstOrigin.y == firstTopRight.y ||
+            secondTopRight.x == secondOrigin.x || secondOrigin.y == secondTopRight.y) {
+            return false
+        }
+        // If one rectangle is on left side of other
+        if (firstOrigin.x > secondTopRight.x || secondOrigin.x > firstTopRight.x) {
+            return false
+        }
+        // If one rectangle is above other
+        if (firstOrigin.y > secondTopRight.y || secondOrigin.y > firstTopRight.y) {
+            return false
+        }
+        
+        return true
+    }
+    
+    func getTopRightPoint(_ rectangle: Rectangle) -> Point {
+        return Point(x: rectangle.origin.x + rectangle.size.width,
+                     y: rectangle.origin.y + rectangle.size.height)
+    }
+    
+    func toString() {
+        print("Rectangle center point: \(self.center)\n" +
+              "Rectangle origin point: \(self.origin)\n" +
+              "Rectangle size: \(self.size.width) width, \(self.size.height) height.\n")
+    }
+    
+    static func zeroAxisDistance(from point: Point) -> Double {
+        sqrt(pow(point.x, 2) + pow(point.y, 2))
+    }
+    
+    static func pointToPointDistance(from point1: Point, to point2: Point) -> Double {
+        sqrt(pow(point2.x - point1.x, 2) + pow(point2.y - point1.y, 2))
+    }
+}
+
+var rectangle = Rectangle()
+// size of each edge
+rectangle.size = Size(width: 25.0, height: 15.0)
+// set the center Point of the rectangle using the setter of computed variable center
+rectangle.center = Point(x: 0.0, y: 0.0)
+rectangle.toString()
+// set the new center Point using mutating func moveBy
+rectangle.moveBy(x: 3.0, y: 16.0)
+
+var rectangle2 = Rectangle()
+rectangle2.size = Size(width: 15.0, height: 10.0)
+rectangle2.center = Point(x: 17.5, y: 10) // rectangle2 - overlapping case with rectangle
+rectangle2.toString()
+rectangle.overlaps(rectangle2)
+rectangle2.center = Point(x: 14.40, y: 2.76) // rectangle2 - not overlapping case
+rectangle.overlaps(rectangle2)
+
+Rectangle.zeroAxisDistance(from: Point(x: 12.4, y: -33.2)) // 35.44 Euclidean distance from start of Axis
+Rectangle.pointToPointDistance(from: Point(x: 12.4, y: -33.2), to: Point(x: 32.4, y: -55.2))
 
 //3.6 Phone and battery
 //Create a type to represent a Phone, it should have a name, a batteryCharge and a display resolution as properties.
