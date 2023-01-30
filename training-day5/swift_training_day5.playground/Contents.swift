81a9333ab47e129c5bcf3311acf5e1465ab5c6ba
Lefteris Kostakis <lp.kostakis@yahoo.gr>
2023-01-30 11:53:48 +0200

    init day 5

---
+++ training-day5/swift_training_day5.playground/Contents.swift
@@ -0,0 +1,57 @@
+//Swift Training Day 5 - Exercises
+import Foundation
+import UIKit
+
+//5.0
+class Cat {
+}
+var things: [Any] = [0, 0.0, (0,0), "0,0", 42, { (name: String) -> String in "Hello, \(name)" }, Cat(), { arc4random() }, { print("Hello") }, UIButton(), {}, [], NSString(), UILabel().self]
+//Iterate thru the array and bind their matched value to a constant of the specified type to enable its value to be printed (If it a function then call it)
+//Implement it both with if statement and with case pattern matching
+
+
+//5.1
+//Create MusicViewController,AlbumViewController subclass of UIViewController
+//Create and array that holds the types of MusicViewController & MusicViewController, give that array explicit type annotation
+//Create a method that takes that array and returns instances of the controllers
+
+//5.2
+//Create a class Dog with a name property and an initializer.
+//Create a makePuppy method that takes a String and returns an instance of the same type. (e.g. if it is called on Dog instance will create new Dog instance)
+//Create two subclasses of that Dog class
+
+//5.3
+//Create a UIColor exention to add an initializer that creates a color from a hex string e.g "#66CCCC
+
+//5.4
+//Create a UIColor exention with the 4 basic colors of our Prooduct application. These should be constants and static, because their value will not change and will be the same across all instance of UIColor
+
+//5.5
+//Extend the Date class to add some convenience methods to calculate time units using DateComponents.
+//The methods to add plusMonths plusWeeks plusDays plusHours minusMonths minusWeeks minusDays minusHours
+//The methods should replace the current object and return it
+//You should be able to calculate this:
+//let date1 = Date().plusMonths(2).minusDays(5).plusHours(15)
+
+//5.6
+//Create an inverse method that takes a Double an returns the inversed (1/x) value.
+//Call this method with values 1,2,0
+//The method should throw an custom error if the X is zero.
+
+//5.7
+//Create an HttpClient class. It should have two methods, a userLogin that takes a username,password and returns a userID and a getUserProfile that takes a userID and returns a Userprofile(struct with first and last name of the user) .
+//The HttpClient should hold internaly a list of valid username/passwords and their mapping to userIDs and UserProfiles
+//The methods userLogin/getUserProfile should throw the appropriate errors for wrong username/password and wrong userID.
+
+//5.8
+//Add shuffle method to the Array type that performs a random permutation of its items
+
+//5.9
+//Add a replace(target: String, withString: String) -> String method in String type
+
+//5.10
+//Create a class Temperature with a double value to hold the current temp in kelvin.
+//Adopt the protocol ExpressibleByFloatLiteral (implement  init(floatLiteral val: Float){})
+//declare a variable with type Temperature and assing Assign it the value -273.15
+//read the documentation of ExpressibleByFloatLiteral
+
