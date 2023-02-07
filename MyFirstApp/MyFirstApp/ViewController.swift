a22b37644aeb608e92b0bd8c3f176e38131d1a3d
Lefteris Kostakis <lp.kostakis@yahoo.gr>
2023-02-08 00:07:24 +0200

    impl buttons/slider logic

--- MyFirstApp/MyFirstApp/ViewController.swift
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -7,32 +7,58 @@
 
 import UIKit
 
+@propertyWrapper
+struct NumberZeroToTen {
+    private var number: Float
+    
+    var wrappedValue: Float {
+        get { number }
+        set { number = min(max(newValue, 0), 10) }
+    }
+    
+    init(wrappedValue: Float) {
+        number = min(max(wrappedValue, 0), 10)
+    }
+}
+
 class ViewController: UIViewController {
     
-    @IBOutlet weak var value: UILabel!
+    @IBOutlet weak var labelValue: UILabel!
     @IBOutlet weak var minusButton: UIButton!
     @IBOutlet weak var plusButton: UIButton!
     @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var sliderValue: UISlider!
+    
+    @NumberZeroToTen private var counter: Float = 0.0 {
+        didSet {
+            sliderValue.value = round(counter * 100) / 100
+            roundCounter()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func minusOne(_ sender: UIButton) {
-        
+    @IBAction func plusOneButton(_ sender: UIButton) {
+        counter += 1
     }
     
-    @IBAction func plusOne(_ sender: UIButton) {
-        
+    @IBAction func minusOneButton(_ sender: UIButton) {
+        counter -= 1
     }
     
-    @IBAction func reset(_ sender: UIButton) {
-        
+    @IBAction func resetButton(_ sender: UIButton) {
+        counter = 0
     }
     
     @IBAction func adjustSlider(_ sender: UISlider) {
-        
+        counter = sender.value
+    }
+    
+    func roundCounter() {
+        labelValue.text = String(round(counter * 100) / 100)
     }
     
 }
