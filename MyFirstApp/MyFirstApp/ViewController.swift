df3f6a43a2797cdc0bb926b4f49d284dcfe5b71f
Lefteris Kostakis <lp.kostakis@yahoo.gr>
2023-02-10 11:54:26 +0200

    UI elements and connections & slider logic completed

--- MyFirstApp/MyFirstApp/ViewController.swift
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -8,57 +8,43 @@
 import UIKit
 
 @propertyWrapper
-struct NumberZeroToTen {
-    private var number: Float
-    
-    var wrappedValue: Float {
+struct LimitRange {
+    private var number: Int
+    var minValue: Int = 0
+    var maxValue: Int = 100
+    var wrappedValue: Int {
         get { number }
-        set { number = min(max(newValue, 0), 10) }
+        set { number = min(max(newValue, minValue), maxValue) }
     }
     
-    init(wrappedValue: Float) {
-        number = min(max(wrappedValue, 0), 10)
+    init(wrappedValue: Int, minValue: Int, maxValue: Int) {
+        number = min(max(wrappedValue, minValue), maxValue)
+        self.minValue = minValue
+        self.maxValue = maxValue
     }
 }
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var labelValue: UILabel!
-    @IBOutlet weak var minusButton: UIButton!
-    @IBOutlet weak var plusButton: UIButton!
-    @IBOutlet weak var resetButton: UIButton!
-    @IBOutlet weak var sliderValue: UISlider!
-    
-    @NumberZeroToTen private var counter: Float = 0.0 {
-        didSet {
-            sliderValue.value = round(counter * 100) / 100
-            roundCounter()
-        }
-    }
-    
+    @IBOutlet weak var slider: UISlider! // helping var in order to set slider at 50
+    @LimitRange(minValue: 0, maxValue: 100) private var sliderValue: Int = 50
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
-    @IBAction func plusOneButton(_ sender: UIButton) {
-        counter += 1
+    override func viewDidAppear(_ animated: Bool) {
+        startingNewGame()
     }
-    
-    @IBAction func minusOneButton(_ sender: UIButton) {
-        counter -= 1
+
+    func startingNewGame() {
+        slider.value = 50
+        labelValue.text = "Put the Bull's eye as close as you can to: \(Int.random(in: 0...100))"
     }
-    
-    @IBAction func resetButton(_ sender: UIButton) {
-        counter = 0
-    }
-    
+
     @IBAction func adjustSlider(_ sender: UISlider) {
-        counter = sender.value
-    }
-    
-    func roundCounter() {
-        labelValue.text = String(round(counter * 100) / 100)
+        sliderValue = Int(sender.value)
     }
     
 }
