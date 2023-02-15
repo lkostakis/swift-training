6afbe1db1b09fd860217e079dde0c33103348bbc
Lefteris Kostakis <lp.kostakis@yahoo.gr>
2023-02-16 01:48:16 +0200

    pickerView done

--- MyFirstApp/MyFirstApp/SettingsViewController.swift
+++ MyFirstApp/MyFirstApp/SettingsViewController.swift
@@ -8,36 +8,50 @@
 import UIKit
 
 class SettingsViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-    let pickerViewRows = ["First row,", "Secound row,","Third row,","Fourth row"]
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        3
-    }
+
+    private var currentLevel = 0
+    
+    let pickerViewRows = ["I'm too young to die (1-30)", "Hey, not too rough (1-100)", "Hurt me plenty (1-200)", "Ultra-Violence (1-300)", "Nightmare!  (1-1000)"]
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        3
+        self.pickerViewRows.count
     }
     
-
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        self.pickerViewRows[row]
+    }
     @IBOutlet weak var pickerView: UIPickerView!
+    
+    init(currentLevel level: Int) {
+        self.currentLevel = level
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         pickerView.delegate = self
         pickerView.dataSource = self
-
-        // Do any additional setup after loading the view.
+        pickerView.selectRow(selectRow(currentLevel), inComponent: 0, animated: true)
+        
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func selectRow(_ level: Int) -> Int {
+        switch currentLevel {
+        case 30: return 0
+        case 100: return 1
+        case 200: return 2
+        case 300: return 3
+        default: return 4
+        }
     }
-    */
-
+    
 }
--- MyFirstApp/MyFirstApp/ViewController.swift
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -7,37 +7,24 @@
 
 import UIKit
 
-@propertyWrapper
-struct LimitRange {
-    private var number: Int
-    var minValue: Int = 0
-    var maxValue: Int = 100
-    var wrappedValue: Int {
-        get { number }
-        set { number = min(max(newValue, minValue), maxValue) }
-    }
-    
-    init(wrappedValue: Int, minValue: Int, maxValue: Int) {
-        number = min(max(wrappedValue, minValue), maxValue)
-        self.minValue = minValue
-        self.maxValue = maxValue
-    }
-}
-
 class ViewController: UIViewController {
     
+    enum DifficultyLevel : Int {
+        case easy = 30, medium = 100, hard = 200, veryHard = 300, ultraHard = 1000
+    }
+    private var selectedLevel = DifficultyLevel.medium.rawValue
+    @IBOutlet weak var maxValueLabel: UILabel!
     @IBOutlet weak var labelValue: UILabel!
     @IBOutlet weak var slider: UISlider! // helping var in order to set slider at 50
-    @LimitRange(minValue: 1, maxValue: 100) private var sliderValue: Int = 50
+    private lazy var sliderValue = selectedLevel
     private var targetValue: Int = 0
     private var computeScore: Int {
-        get { 100 - abs(Int(slider.value) - targetValue) }
-    }
+        get { (selectedLevel - abs(Int(slider.value) - targetValue))*100/selectedLevel }}
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var roundLabel: UILabel!
     private lazy var totalScore: (total: Int, label: UILabel) = (0, scoreLabel)
     private lazy var roundCounter: (counter: Int, label: UILabel) = (0, roundLabel)
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Bull's Eye"
@@ -51,13 +38,16 @@ class ViewController: UIViewController {
         super.viewWillAppear(animated)
         startNextRound()
     }
-
+    
     func startNextRound() {
-        slider.value = 50 // init the slider to the middle
+        maxValueLabel.text = String(selectedLevel)
+        slider.maximumValue = Float(selectedLevel)
+        slider.minimumValue = 1
+        slider.value = Float(selectedLevel/2) // init the slider to the middle
         roundCounter.counter += 1 // to the next round..
         roundCounter.label.text = "Round: \(roundCounter.counter)"
         totalScore.label.text = "Score: \(totalScore.total)"
-        targetValue = Int.random(in: 1...100)
+        targetValue = Int.random(in: 1...selectedLevel)
         labelValue.text = "Put the Bull's eye as close as you can to: \(targetValue)"
     }
 
@@ -66,7 +56,7 @@ class ViewController: UIViewController {
             title: "Scoreboard",
             message: "Your score is: \(computeScore)",
             preferredStyle: .alert)
-        
+
         let action = UIAlertAction(
             title: "OK",
             style: .default,
@@ -105,6 +95,7 @@ class ViewController: UIViewController {
     
     @IBAction func adjustSlider(_ sender: UISlider) {
         sliderValue = Int(sender.value)
+//        print(sliderValue)
     }
     
     @IBAction func infoButtonTapped(_ sender: UIButton) {
@@ -115,10 +106,9 @@ class ViewController: UIViewController {
     
     @objc func settingsTapped() {
         print("tapped")
-        let settingsViewController = SettingsViewController()
+        let settingsViewController = SettingsViewController(currentLevel: selectedLevel)
         settingsViewController.modalPresentationStyle = .fullScreen
         navigationController?.pushViewController(settingsViewController, animated: true)
     }
-    
-}
 
+}
