179d9d7dd9279464acfdee68841771e298e1ff36
Lefteris Kostakis <lp.kostakis@yahoo.gr>
2023-02-20 10:19:06 +0200

    AboutViewController--description changed

--- MyFirstApp/MyFirstApp/AboutViewController.swift
+++ MyFirstApp/MyFirstApp/AboutViewController.swift
@@ -16,7 +16,7 @@ class AboutViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        textViewDescription.text = "The objective of the game is to move the slider as close to a randomly chosen target value as you can. In the screenshot, the aim is to put the bull’s eye at 62. Because you can’t see the current value of the slider, you’ll have to “eyeball” it."
+        textViewDescription.text = "The objective of the game is to move a slider as close to a randomly chosen target value as you can. Because you can’t see the current value of the slider, you’ll have to “eyeball” it. There are five different difficulty levels, each of which re-evaluates the slider's values in different ranges starting from 1, such as [1, 30], [1, 100], etc."
     }
     
 }
