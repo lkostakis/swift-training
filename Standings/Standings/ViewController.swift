207957cf31e4ba6908bc3701d25fd6f6d1dd1037
Lefteris Kostakis <lp.kostakis@yahoo.gr>
2023-02-22 23:23:27 +0200

    init project Standings

---
+++ Standings/Standings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Standings
+//
+//  Created by Lefteris Kostakis on 22/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
